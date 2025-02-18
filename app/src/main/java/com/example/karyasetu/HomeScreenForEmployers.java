2f29d21dba3dfd44d1494f0678990fc2fa90dcc2
Sumeet7774 <sumeetkapadia7@gmail.com>
2025-02-18 17:46:00 +0530

    Created all the fragments For both jobseeker and employer. Now the bottom navigation is working correctly along with its animation. Also added few files and also made some changes.

--- app/src/main/java/com/example/karyasetu/HomeScreenForEmployers.java
+++ app/src/main/java/com/example/karyasetu/HomeScreenForEmployers.java
@@ -1,14 +1,24 @@
 package com.example.karyasetu;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeScreenForEmployers extends AppCompatActivity {
+
+    SessionManagement sessionManagement;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +26,86 @@ public class HomeScreenForEmployers extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_screen_for_employers);
 
+        sessionManagement = new SessionManagement(this);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationMenu_emp);
+
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragmentForEmployer(), true, false);
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                Fragment currentFragment = getCurrentFragment();
+                boolean isNavigatingBack = false;
+
+                if (itemId == R.id.home_menu_emp) {
+                    if (!(currentFragment instanceof HomeFragmentForEmployer)) {
+                        isNavigatingBack = currentFragment != null &&
+                                (currentFragment instanceof MyJobPostsFragmentForEmployer ||
+                                        currentFragment instanceof ApplicantsFragmentForEmployer ||
+                                        currentFragment instanceof ProfileFragmentForEmployer);
+                        loadFragment(new HomeFragmentForEmployer(), false, isNavigatingBack);
+                    }
+                } else if (itemId == R.id.myjobposts_emp) {
+                    if (!(currentFragment instanceof MyJobPostsFragmentForEmployer)) {
+                        isNavigatingBack = currentFragment != null &&
+                                (currentFragment instanceof ApplicantsFragmentForEmployer ||
+                                        currentFragment instanceof ProfileFragmentForEmployer);
+                        loadFragment(new MyJobPostsFragmentForEmployer(), false, isNavigatingBack);
+                    }
+                } else if (itemId == R.id.applicants_menu_emp) {
+                    if (!(currentFragment instanceof ApplicantsFragmentForEmployer)) {
+                        isNavigatingBack = currentFragment != null &&
+                                (currentFragment instanceof ProfileFragmentForEmployer);
+                        loadFragment(new ApplicantsFragmentForEmployer(), false, isNavigatingBack);
+                    }
+                } else if (itemId == R.id.profile_menu_emp) {
+                    if (!(currentFragment instanceof ProfileFragmentForEmployer)) {
+                        loadFragment(new ProfileFragmentForEmployer(), false, false);
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+    private void loadFragment(Fragment newFragment, boolean isAppInitialized, boolean isNavigatingBack) {
+        Fragment currentFragment = getCurrentFragment();
+        loadFragment(newFragment, currentFragment, isAppInitialized, isNavigatingBack);
+    }
+
+    private void loadFragment(Fragment newFragment, Fragment currentFragment, boolean isAppInitialized, boolean isNavigatingBack) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (currentFragment != null) {
+            if (isNavigatingBack) {
+                fragmentTransaction.setCustomAnimations(
+                        R.anim.slide_in_left, R.anim.slide_out_right,
+                        R.anim.slide_in_right, R.anim.slide_out_left
+                );
+            } else {
+                fragmentTransaction.setCustomAnimations(
+                        R.anim.slide_in_right, R.anim.slide_out_left,
+                        R.anim.slide_in_left, R.anim.slide_out_right
+                );
+            }
+        }
+
+        if (isAppInitialized && fragmentManager.findFragmentById(R.id.frameLayout) == null) {
+            fragmentTransaction.add(R.id.frameLayout, newFragment);
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, newFragment);
+        }
+
+        fragmentTransaction.commit();
+    }
+
+    private Fragment getCurrentFragment()
+    {
+        return getSupportFragmentManager().findFragmentById(R.id.frameLayout);
     }
 }
--- app/src/main/java/com/example/karyasetu/HomeScreenForJobSeekers.java
+++ app/src/main/java/com/example/karyasetu/HomeScreenForJobSeekers.java
@@ -1,20 +1,104 @@
 package com.example.karyasetu;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeScreenForJobSeekers extends AppCompatActivity {
+
+    SessionManagement sessionManagement;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_screen_for_job_seekers);
 
+        sessionManagement = new SessionManagement(this);
+        bottomNavigationView = findViewById(R.id.bottomNavigationMenu_job);
+
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragmentForJobSeeker(), true, false);
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                Fragment currentFragment = getCurrentFragment();
+                boolean isNavigatingBack = false;
+
+                if (itemId == R.id.home_menu_job) {
+                    if (!(currentFragment instanceof HomeFragmentForJobSeeker)) {
+                        isNavigatingBack = currentFragment != null &&
+                                (currentFragment instanceof JobListingsFragmentForJobSeeker ||
+                                        currentFragment instanceof ApplicationsFragmentForJobSeeker ||
+                                        currentFragment instanceof ProfileFragmentForJobSeeker);
+                        loadFragment(new HomeFragmentForJobSeeker(), false, isNavigatingBack);
+                    }
+                } else if (itemId == R.id.job_menu_job) {
+                    if (!(currentFragment instanceof JobListingsFragmentForJobSeeker)) {
+                        isNavigatingBack = currentFragment != null &&
+                                (currentFragment instanceof ApplicationsFragmentForJobSeeker ||
+                                        currentFragment instanceof ProfileFragmentForJobSeeker);
+                        loadFragment(new JobListingsFragmentForJobSeeker(), false, isNavigatingBack);
+                    }
+                } else if (itemId == R.id.application_menu_job) {
+                    if (!(currentFragment instanceof ApplicationsFragmentForJobSeeker)) {
+                        isNavigatingBack = currentFragment != null &&
+                                (currentFragment instanceof ProfileFragmentForJobSeeker);
+                        loadFragment(new ApplicationsFragmentForJobSeeker(), false, isNavigatingBack);
+                    }
+                } else if (itemId == R.id.profile_menu_job) {
+                    if (!(currentFragment instanceof ProfileFragmentForJobSeeker)) {
+                        loadFragment(new ProfileFragmentForJobSeeker(), false, false);
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+    private void loadFragment(Fragment newFragment, boolean isAppInitialized, boolean isNavigatingBack) {
+        Fragment currentFragment = getCurrentFragment();
+        loadFragment(newFragment, currentFragment, isAppInitialized, isNavigatingBack);
+    }
+
+    private void loadFragment(Fragment newFragment, Fragment currentFragment, boolean isAppInitialized, boolean isNavigatingBack) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (currentFragment != null) {
+            if (isNavigatingBack) {
+                fragmentTransaction.setCustomAnimations(
+                        R.anim.slide_in_left, R.anim.slide_out_right,
+                        R.anim.slide_in_right, R.anim.slide_out_left
+                );
+            } else {
+                fragmentTransaction.setCustomAnimations(
+                        R.anim.slide_in_right, R.anim.slide_out_left,
+                        R.anim.slide_in_left, R.anim.slide_out_right
+                );
+            }
+        }
+
+        if (isAppInitialized && fragmentManager.findFragmentById(R.id.frameLayout) == null) {
+            fragmentTransaction.add(R.id.frameLayout, newFragment);
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, newFragment);
+        }
+
+        fragmentTransaction.commit();
+    }
+
+    private Fragment getCurrentFragment() {
+        return getSupportFragmentManager().findFragmentById(R.id.frameLayout);
     }
 }
